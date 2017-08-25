527c9be6830016d7ce9464dd0be959bbaab2be84
Alex Ellwein <alex.ellwein@gmail.com>
2017-08-25 17:02:46 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AlcoholCalculatorComponent} from './alcohol-calculator/alcohol-calculator.component';
+import {FormsModule} from '@angular/forms';
+import {CalcService} from './calc.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlcoholCalculatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    CalcService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
