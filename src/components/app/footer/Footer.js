3e5e2389ab19f17738d531a842cd14b6a003e23f
Alex Ellwein <alex.ellwein@gmail.com>
2020-08-26 18:40:31 +0200

    i18nize footer

--- src/components/app/footer/Footer.js
+++ src/components/app/footer/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import './Footer.css';
 
 const Footer = () => {
+    const { t } = useTranslation();
     return (
         <footer className="footer-thin">
             <div className="content has-text-centered">
-                <a href="https://github.com/aellwein/beercalc">Project on GitHub</a> | Created by <a href="https://github.com/aellwein">Alex Ellwein</a> | Licensed under the <a href="/license.html">MIT License</a>
+                <a href="https://github.com/aellwein/beercalc">{t('project on github')}</a> | {t('licensed under')} <a href="/license.html">{t('mit license')}</a>
             </div>
         </footer>
     );
