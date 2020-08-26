aa5a8abb069e5081ea41e55a0bb689dac5b9fc81
Alex Ellwein <alex.ellwein@gmail.com>
2020-08-26 17:29:24 +0200

    extract  i18n logic in a separate component

---
+++ src/components/lang-picker/LangPicker.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useTranslation } from "react-i18next";
+
+const languages = function* () {
+    const langs = [
+        { 'short': 'en', 'long': 'English' },
+        { 'short': 'de', 'long': 'Deutsch' },
+        { 'short': 'ru', 'long': 'Русский' },
+    ];
+    for (let i of langs) {
+        yield <option key={i.short} value={i.short}>{i.long}</option>
+    }
+}
+
+const onChangeLang = (lang, i18n) => {
+    i18n.changeLanguage(lang);
+}
+
+const LangPicker = (_) => {
+    const { i18n } = useTranslation();
+
+    return (
+        <div className="is-right">
+            <div className="select">
+                <select onChange={e => onChangeLang(e.target.value, i18n)} value={i18n.language}>
+                    {[...languages()]}
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default LangPicker;
