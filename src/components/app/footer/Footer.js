bfaaa81852d5013d7042c5e71026b44ddef5ba8a
Alex Ellwein <alex.ellwein@gmail.com>
2020-12-20 15:29:35 +0100

    show git version / commit in the footer

--- src/components/app/footer/Footer.js
+++ src/components/app/footer/Footer.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './Footer.css';
+import GitInfo from 'react-git-info/macro';
 
 const Footer = () => {
     const { t } = useTranslation();
+    const baseUrl = "https://github.com/aellwein/beercalc";
+    const gitInfo = GitInfo();
     return (
         <footer className="footer-thin">
             <div className="content has-text-centered">
-                <a href="https://github.com/aellwein/beercalc">{t('project on github')}</a> | {t('licensed under')} <a href="/license.html">{t('mit license')}</a>
+                <a href={baseUrl}>{t('project on github')}</a> | &nbsp;
+                {t('version')} <a href={baseUrl + "/releases/tag/" + gitInfo.tags[0]}>{gitInfo.tags[0]}</a> | &nbsp;
+                {t('commit')} <a href={baseUrl + "/commit/" + gitInfo.commit.hash}>{gitInfo.commit.shortHash}</a> | &nbsp;
+            {t('licensed under')} <a href="/license.html">{t('mit license')}</a>
             </div>
-        </footer>
+        </footer >
     );
 }
 
