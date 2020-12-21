2f680a2e83df7ba6a5f5af5010649cc2e1c84a8a
Alex Ellwein <alex.ellwein@gmail.com>
2020-12-21 11:36:12 +0100

    improve git version info for unpublished versions

--- src/components/app/footer/Footer.js
+++ src/components/app/footer/Footer.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import GitInfo from 'react-git-info/macro';
 import { useTranslation } from 'react-i18next';
 import './Footer.css';
-import GitInfo from 'react-git-info/macro';
+
+const baseUrl = "https://github.com/aellwein/beercalc";
+
+
+const version = (gitInfo, t) => {
+    if (gitInfo.branch !== 'master' || gitInfo.tags.length < 1) {
+        return <span>{t('developer version')}</span>;
+    } else {
+        return <span>{t('version')} <a href={baseUrl + "/releases/tag/" + gitInfo.tags[0]}>{gitInfo.tags[0]}</a></span>
+    }
+}
+
+const commit = (gitInfo, t) => {
+    if (gitInfo.branch !== 'master' || gitInfo.tags.length < 1) {
+        return <span>{t('commit')} {gitInfo.commit.shortHash}</span>;
+    } else {
+        return <span>{t('commit')} <a href={baseUrl + "/commit/" + gitInfo.commit.hash}>{gitInfo.commit.shortHash}</a></span>;
+    }
+}
+
 
 const Footer = () => {
-    const { t } = useTranslation();
-    const baseUrl = "https://github.com/aellwein/beercalc";
     const gitInfo = GitInfo();
+    const { t } = useTranslation();
     return (
         <footer className="footer-thin">
             <div className="content has-text-centered">
                 <a href={baseUrl}>{t('project on github')}</a> | &nbsp;
-                {t('version')} <a href={baseUrl + "/releases/tag/" + gitInfo.tags[0]}>{gitInfo.tags[0]}</a> | &nbsp;
-                {t('commit')} <a href={baseUrl + "/commit/" + gitInfo.commit.hash}>{gitInfo.commit.shortHash}</a> | &nbsp;
+                {version(gitInfo, t)} | &nbsp;
+                {commit(gitInfo, t)} | &nbsp;
             {t('licensed under')} <a href="/license.html">{t('mit license')}</a>
             </div>
         </footer >
