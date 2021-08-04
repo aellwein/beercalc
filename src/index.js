fe3f649a7bf9a67f079936a4da1e0f06adad371d
Alex Ellwein <alex.ellwein@gmail.com>
2021-08-04 14:27:20 +0200

    removed bulma, add tailwind css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import 'bulma/css/bulma.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/app/App';
 import './i18n';
+import './index.css';
 import reducers from './reducers';
 
 const store = createStore(
