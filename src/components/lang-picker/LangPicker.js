a06d1d4b75673006978990a224cdf9036a61572a
Alex Ellwein <alex.ellwein@gmail.com>
2021-08-13 12:53:08 +0200

    improve language detection on different variants
    As most of the browsers return "<lang>-<variant>" for accepted
    languages, we need to handle this properly in LangPicker component.
    This fixes #23.

--- src/components/lang-picker/LangPicker.js
+++ src/components/lang-picker/LangPicker.js
@@ -16,12 +16,19 @@ const onChangeLang = (lang, i18n) => {
     i18n.changeLanguage(lang);
 }
 
+const getLang = (lang) => {
+    if (lang.indexOf("-") === -1) {
+        return lang;
+    }
+    return lang.split('-')[0].toLowerCase();
+}
+
 const LangPicker = (_) => {
     const { i18n } = useTranslation();
 
     return (
         <div>
-            <select className="border-gray-300 p-1 border-solid border-1 dark:bg-gray-700 dark:text-gray-300" onChange={e => onChangeLang(e.target.value, i18n)} value={i18n.language}>
+            <select className="border-gray-300 p-1 border-solid border-1 dark:bg-gray-700 dark:text-gray-300" onChange={e => onChangeLang(e.target.value, i18n)} value={getLang(i18n.language)}>
                 {[...languages()]}
             </select>
         </div>
