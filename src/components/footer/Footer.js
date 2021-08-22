4eda4b2f1124c2754b36eff5e7760ce1b46a83d7
Alex Ellwein <alex.ellwein@gmail.com>
2021-08-22 20:34:00 +0200

    adapt footer to show commit hash with link only

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,17 +4,8 @@ import { useTranslation } from 'react-i18next';
 
 const baseUrl = "https://github.com/aellwein/beercalc";
 
-
-const version = (gitInfo, t) => {
-    if (gitInfo.branch !== 'master' || gitInfo.tags.length < 1) {
-        return <span>{t('developer version')}</span>;
-    } else {
-        return <span>{t('version')} <a className="text-indigo-600 hover:underline" href={baseUrl + "/releases/tag/" + gitInfo.tags[0]}>{gitInfo.tags[0]}</a></span>
-    }
-}
-
 const commit = (gitInfo, t) => {
-    if (gitInfo.branch !== 'master' || gitInfo.tags.length < 1) {
+    if (gitInfo.branch !== 'master') {
         return <span>{t('commit')} {gitInfo.commit.shortHash}</span>;
     } else {
         return <span>{t('commit')} <a className="text-indigo-600 hover:underline" href={baseUrl + "/commit/" + gitInfo.commit.hash}>{gitInfo.commit.shortHash}</a></span>;
@@ -27,7 +18,6 @@ const Footer = () => {
     return (
         <footer className="my-6 text-center dark:text-gray-400">
             <a className="text-indigo-600 hover:underline" href={baseUrl}>{t('project on github')}</a> | &nbsp;
-            {version(gitInfo, t)} | &nbsp;
             {commit(gitInfo, t)} | &nbsp;
             {t('licensed under')} <a className="text-indigo-600 hover:underline" href="/license.html">{t('mit license')}</a>
         </footer >
