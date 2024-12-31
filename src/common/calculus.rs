d6a4749ba4b761273df7f58040d9e2ea59885061
Alex Ellwein <alex.ellwein@gmail.com>
2024-12-31 11:14:48 +0100

    feat: implement alc calculator in Rust + WASM

--- src/common/calculus.rs
+++ src/common/calculus.rs
@@ -1,3 +1,6 @@
+use crate::common::prelude::*;
+use anyhow::{anyhow, Result};
+
 pub fn brix_to_plato(brix: f64) -> f64 {
     brix / 1.03
 }
@@ -44,4 +47,164 @@ pub fn plato_to_oechsle(plato: f64) -> f64 {
 
 pub fn sg_to_oechsle(sg: f64) -> f64 {
     brix_to_oechsle(sg_to_brix(sg))
+}
+
+pub struct AlcCalcResult {
+    pub apparent_extract: f64,
+    pub real_extract: f64,
+    pub apparent_attenuation: f64,
+    pub real_attenuation: f64,
+    pub alcohol_by_weight: f64,
+    pub alcohol_by_volume: f64,
+}
+
+pub fn calc_apparent_extract_sg(
+    original_gravity: &Gravity,
+    final_gravity: &Gravity,
+) -> Result<f64> {
+    let og_plato = original_gravity.to_plato().value();
+    let fg_brix = final_gravity.to_brix().value();
+
+    if og_plato == 0.0 {
+        return Err(anyhow!("original gravity is zero"));
+    }
+
+    let apparent_extract_sg = 1.001843
+        - 0.002318474 * og_plato
+        - 0.000007775 * og_plato.powi(2)
+        - 0.000000034 * og_plato.powi(3)
+        + 0.00574 * fg_brix
+        + 0.00003344 * fg_brix.powi(2)
+        + 0.000000086 * fg_brix.powi(3);
+
+    Ok(apparent_extract_sg)
+}
+
+pub fn calc_apparent_extract_sg_terrill(
+    original_gravity: &Gravity,
+    final_gravity: &Gravity,
+) -> Result<f64> {
+    let og_plato = original_gravity.to_plato().value();
+    let fg_plato = final_gravity.to_plato().value();
+    if og_plato == 0.0 {
+        return Err(anyhow!("original gravity is zero"));
+    }
+    if og_plato <= fg_plato {
+        return Err(anyhow!(
+            "original gravity is less or equal than final gravity"
+        ));
+    }
+    Ok(1.0000 - 0.00085683 * og_plato + 0.0034941 * fg_plato)
+}
+
+pub fn calc_real_extract(original_gravity: &Gravity, apparent_extract_plato: f64) -> f64 {
+    0.1808 * original_gravity.to_plato().value() + 0.8192 * apparent_extract_plato
+}
+
+pub fn calc_apparent_attenuation(
+    original_gravity: &Gravity,
+    apparent_extract_plato: f64,
+) -> Result<f64> {
+    let og_plato = original_gravity.to_plato().value();
+    if og_plato == 0.0 {
+        return Err(anyhow!("original gravity is zero"));
+    }
+    Ok((1.0 - apparent_extract_plato / og_plato) * 100.0)
+}
+
+pub fn calc_real_attenuation(original_gravity: &Gravity, real_extract_plato: f64) -> Result<f64> {
+    let og_plato = original_gravity.to_plato().value();
+    if og_plato == 0.0 {
+        return Err(anyhow!("original gravity is zero"));
+    }
+    Ok((1.0 - real_extract_plato / og_plato) * 100.0)
+}
+
+pub fn calc_alcohol_by_weight(original_gravity: &Gravity, real_extract_plato: f64) -> Result<f64> {
+    let og_plato = original_gravity.to_plato().value();
+    if og_plato == 0.0 {
+        return Err(anyhow!("original gravity is zero"));
+    }
+    if (og_plato - real_extract_plato) <= 0.0 {
+        return Err(anyhow!("gravity difference is less or equal zero"));
+    }
+    let div = 2.0665 - (0.010665 * og_plato);
+    if div <= 0.0 {
+        return Err(anyhow!("division by zero"));
+    }
+    Ok((og_plato - real_extract_plato) / div)
+}
+
+pub fn calc_alcohol_by_volume(original_gravity: &Gravity, real_extract_plato: f64) -> Result<f64> {
+    let og_plato = original_gravity.to_plato().value();
+    if og_plato == 0.0 {
+        return Err(anyhow!("original gravity is zero"));
+    }
+    if (og_plato - real_extract_plato) <= 0.0 {
+        return Err(anyhow!("gravity difference is less or equal zero"));
+    }
+    let div = 2.0665 - (0.010665 * og_plato);
+    if div <= 0.0 {
+        return Err(anyhow!("division by zero"));
+    }
+    Ok(1.0 / 0.79 * (og_plato - real_extract_plato) / div)
+}
+
+pub fn calc_alcohol_standard_equation(
+    original_gravity: &Gravity,
+    final_gravity: &Gravity,
+) -> Result<AlcCalcResult> {
+    let apparent_extract_plato;
+    if matches!(original_gravity, Gravity::Brix(_)) {
+        // refractometer method is used here with SG conversion from Brix units
+        let apparent_extract_sg = calc_apparent_extract_sg(&original_gravity, &final_gravity)?;
+        apparent_extract_plato = sg_to_plato(apparent_extract_sg);
+    } else if matches!(final_gravity, Gravity::Plato(_)) {
+        // standard method for calculation with Plato units
+        apparent_extract_plato = final_gravity.to_plato().value();
+    } else {
+        // calculation not possible for this combination of units
+        return Err(anyhow!("calculation not possible"));
+    }
+    let real_extract_plato = calc_real_extract(&original_gravity, apparent_extract_plato);
+    let apparent_attenuation_plato =
+        calc_apparent_attenuation(&original_gravity, apparent_extract_plato)?;
+    let real_attenuation_plato = calc_real_attenuation(&original_gravity, real_extract_plato)?;
+    let alcohol_by_weight = calc_alcohol_by_weight(&original_gravity, real_extract_plato)?;
+    let alcohol_by_volume = calc_alcohol_by_volume(&original_gravity, real_extract_plato)?;
+
+    Ok(AlcCalcResult {
+        apparent_extract: apparent_extract_plato,
+        real_extract: real_extract_plato,
+        apparent_attenuation: apparent_attenuation_plato,
+        real_attenuation: real_attenuation_plato,
+        alcohol_by_weight,
+        alcohol_by_volume,
+    })
+}
+
+pub fn calc_alcohol_terrill_equation(
+    original_gravity: &Gravity,
+    final_gravity: &Gravity,
+) -> Result<AlcCalcResult> {
+    if !matches!(original_gravity, Gravity::Brix(_)) {
+        return Err(anyhow!("this equation requires Brix units"));
+    }
+    let apparent_extract_sg = calc_apparent_extract_sg_terrill(&original_gravity, &final_gravity)?;
+    let apparent_extract_plato = sg_to_plato(apparent_extract_sg);
+    let real_extract_plato = calc_real_extract(&original_gravity, apparent_extract_plato);
+    let apparent_attenuation_plato =
+        calc_apparent_attenuation(&original_gravity, apparent_extract_plato)?;
+    let real_attenuation_plato = calc_real_attenuation(&original_gravity, real_extract_plato)?;
+    let alcohol_by_weight = calc_alcohol_by_weight(&original_gravity, real_extract_plato)?;
+    let alcohol_by_volume = calc_alcohol_by_volume(&original_gravity, real_extract_plato)?;
+
+    Ok(AlcCalcResult {
+        apparent_extract: apparent_extract_plato,
+        real_extract: real_extract_plato,
+        apparent_attenuation: apparent_attenuation_plato,
+        real_attenuation: real_attenuation_plato,
+        alcohol_by_weight,
+        alcohol_by_volume,
+    })
 }
--- src/components/alcohol_calc.rs
+++ src/components/alcohol_calc.rs
@@ -14,8 +14,94 @@ pub fn AlcoholCalculator() -> Html {
                 <div class="text-2xl my-3">{t.t("alcohol calculator", &state.language)}</div>
                 <UnitPicker />
                 <GravityPicker />
+                <div class="flex flex-row flex-wrap gap-4">
+                    <AlcCalc equation={EquationType::Standard} />
+                    { if matches!(state.chosen_unit, Gravity::Brix(_)) {
+                        html! {
+                            <AlcCalc equation={EquationType::Terrill} />
+                        }
+                    } else { html! {} }
+                    }
+                </div>
             </div>
             <Footer />
         </>
+    }
+}
+
+#[derive(Clone, PartialEq)]
+enum EquationType {
+    Standard,
+    Terrill,
+}
+
+#[derive(Properties, Clone, PartialEq)]
+struct CalcResultProps {
+    equation: EquationType,
+}
+
+#[function_component]
+fn AlcCalc(props: &CalcResultProps) -> Html {
+    let (state, _) = use_store::<CalcState>();
+    let t = use_context::<Translator>().unwrap();
+    let title = match props.equation {
+        EquationType::Standard => {
+            if matches!(state.chosen_unit, Gravity::Brix(_)) {
+                t.t("standard equation", &state.language)
+            } else {
+                t.t("results", &state.language)
+            }
+        }
+        EquationType::Terrill => t.t("terrill equation", &state.language),
+    };
+    let calc_result = match props.equation {
+        EquationType::Standard => {
+            calc_alcohol_standard_equation(&state.original_gravity, &state.final_gravity)
+        }
+        EquationType::Terrill => {
+            calc_alcohol_terrill_equation(&state.original_gravity, &state.final_gravity)
+        }
+    };
+    html! {
+        <div class="flex flex-wrap flex-col shadow-md p-4 gap-2 flex-grow">
+            <div class="text-2xl text-center my-3">{title}</div>
+            {
+                if let Ok(result) = calc_result {
+                    html! {
+                        <>
+                        <div class="flex flex-row flex-wrap gap-2 hover:border-b hover:border-neutral-700 dark:hover:border-neutral-500">
+                            <div class="text-left flex-grow">{t.t("apparent extract", &state.language)}{":"}</div>
+                            <div class="text-right">{format!(r#"{:.1}"#, result.apparent_extract)}{" °P"}</div>
+                        </div>
+                        <div class="flex flex-row flex-wrap gap-2 hover:border-b hover:border-neutral-700 dark:hover:border-neutral-500">
+                            <div class="text-left flex-grow">{t.t("real extract", &state.language)}{":"}</div>
+                            <div class="text-right">{format!(r#"{:.1}"#, result.real_extract)}{" %"}</div>
+                        </div>
+                        <div class="flex flex-row flex-wrap gap-2 hover:border-b hover:border-neutral-700 dark:hover:border-neutral-500">
+                            <div class="text-left flex-grow">{t.t("apparent attenuation", &state.language)}{":"}</div>
+                            <div class="text-right">{format!(r#"{:.1}"#, result.apparent_attenuation)}{" %"}</div>
+                        </div>
+                        <div class="flex flex-row flex-wrap gap-2 hover:border-b hover:border-neutral-700 dark:hover:border-neutral-500">
+                            <div class="text-left flex-grow">{t.t("real attenuation", &state.language)}{":"}</div>
+                            <div class="text-right">{format!(r#"{:.1}"#, result.real_attenuation)}{" %"}</div>
+                        </div>
+                        <div class="flex flex-row flex-wrap gap-2 hover:border-b hover:border-neutral-700 dark:hover:border-neutral-500">
+                            <div class="text-left flex-grow">{t.t("alcohol by weight", &state.language)}{":"}</div>
+                            <div class="text-right">{format!(r#"{:.1}"#, result.alcohol_by_weight)}{" %"}</div>
+                        </div>
+                        <div class="flex flex-row flex-wrap gap-2 hover:border-b hover:border-neutral-700 dark:hover:border-neutral-500">
+                            <div class="text-left flex-grow">{t.t("alcohol by volume", &state.language)}{":"}</div>
+                            <div class="text-right">{format!(r#"{:.1}"#, result.alcohol_by_volume)}{" %"}</div>
+                        </div>
+
+                        </>
+                    }
+                } else {
+                    html!{
+                        <div class="text-center flex-grow text-red-400 table"><span class="table-cell align-middle">{t.t("calculation not possible", &state.language)}</span></div>
+                    }
+                }
+            }
+        </div>
     }
 }
